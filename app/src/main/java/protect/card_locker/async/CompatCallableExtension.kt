6c29fba63cce698a9feb8866140f10b36d2bbad2
davkenn <davidkennedyster@gmail.com>
2025-08-10 05:05:59 -0700

    added wrapper to make CompatCallable work with coroutines

---
+++ app/src/main/java/protect/card_locker/async/CompatCallableExtension.kt
@@ -0,0 +1,19 @@
+package protect.card_locker.async
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+suspend fun <T> CompatCallable<T>.runSuspending(): T {
+    withContext(
+        Dispatchers.Main
+    ){
+        onPreExecute()
+    }
+    val result: T = withContext(Dispatchers.IO){
+        call()
+    }
+    withContext(Dispatchers.Main){
+        onPostExecute(result)
+    }
+    return result
+}
